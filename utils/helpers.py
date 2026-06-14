a7a228fbd573e6625bb313d67bedda0cd6bf701a
Fevber <nah.id.win.286h@gmail.com>
2026-06-14 00:37:30 -0600

    ssss

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,20 +1,20 @@
 import os
 from datetime import datetime
 
-def h1():
-    os.system('clear' if os.name == 'posix' else 'cls')
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def h2(path):
+def read_file(path):
     try:
         with open(path, 'r') as f:
             return f.read()
     except:
         return None
 
-def h3(path, content):
+def write_file(path, content):
     with open(path, 'w') as f:
         f.write(content)
         
-def h4(message):
+def log_message(message):
     with open("data/logs.txt", "a") as f:
         f.write(f"[{datetime.now()}] {message}\n")
