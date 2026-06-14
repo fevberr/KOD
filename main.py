1c70754f33cda7065113281955221b6625d836bd
Fevber <nah.id.win.286h@gmail.com>
2026-06-14 00:50:00 -0600

    fixed

--- main.py
+++ main.py
@@ -3,19 +3,8 @@ from display.banner import show_banner
 from display.panels import draw_panel
 from config import host, port, device, system, ping
 import os
-import signal
-
-def handle_resize(signum, frame):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    show_banner()
-    info = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
-    draw_panel("23 KOD", info, "READY")
-    menu(True)
 
 def run():
-    if os.name == 'posix':
-        signal.signal(signal.SIGWINCH, handle_resize)
-    
     os.system('cls' if os.name == 'nt' else 'clear')
     show_banner()
     
