0a2bd913d79bba8a9af1937d0f3d22d678a3eda2
Fevber <nah.id.win.286h@gmail.com>
2026-06-18 17:16:03 -0600

    re

--- utils/loader.py
+++ utils/loader.py
@@ -1,17 +1,16 @@
 import os
+import sys
 import subprocess
 
 def l1(m1):
     if m1.endswith('.py'):
         try:
-            subprocess.run(['python3', m1], check=True)
+            p = sys.executable
+            subprocess.run([p, m1], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error running module: {e}")
-        except FileNotFoundError:
-            try:
-                subprocess.run(['python', m1], check=True)
-            except Exception as e:
-                print(f"Error: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
     elif m1.endswith('.lua'):
         try:
             r1 = subprocess.run(['lua', m1], capture_output=True, text=True)
