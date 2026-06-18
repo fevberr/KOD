f689e3f531331702b47988496de24d536535eb8d
Fevber <nah.id.win.286h@gmail.com>
2026-06-17 20:48:51 -0600

    mini bug

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ from loader import f1
 import os
 import sys
 import time
-from config import g1, g2
+from config import g1, g2, host, port, device, system, ping
 
 I1 = 5
 
@@ -41,9 +41,8 @@ def m1():
         
         from display.banner import b1
         from display.panels import p1
-        from config import h1, p2, d1, s1, p3
         b1()
-        i1 = f"host:      {h1}\nPort:        {p2}\nPing:     {p3}\ndevice:   {d1}\nsystem:    {s1}"
+        i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
         p1("23 KOD", i1, "READY")
         
         n2 = n1()
