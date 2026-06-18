2900753dbd6a4b85e4427f38514ad93e7f2eb8d2
Fevber <nah.id.win.286h@gmail.com>
2026-06-17 20:46:37 -0600

    lil fix

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from menu import m1
 from display.banner import b1
 from display.panels import p1
-from config import h1, p2, d1, s1, p3
+from config import host, port, device, system, ping
 import os
 
 def r1():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('clear')
     b1()
     
-    i1 = f"host:      {h1}\nPort:        {p2}\nPing:     {p3}\ndevice:   {d1}\nsystem:    {s1}"
+    i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
     p1("23 KOD", i1, "READY")
     
     m1()
