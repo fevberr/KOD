348247374c52c95a2ab91f8d15e23e0bd472a8d9
Fevber <nah.id.win.286h@gmail.com>
2026-07-19 22:13:03 -0600

    re

--- utils/colors.py
+++ utils/colors.py
@@ -2,7 +2,7 @@ import os
 import sys
 import platform
 
-def a1():
+def col_support():
     if not hasattr(sys.stdout, 'isatty') or not sys.stdout.isatty():
         return False
     if os.environ.get('NO_COLOR'):
@@ -26,8 +26,8 @@ def a1():
             return False
     return term not in ('', 'dumb', 'linux')
 
-def a2():
-    class b1:
+def get_colors():
+    class Colors:
         RS = '\033[0m'
         B = '\033[1m'
         D = '\033[2m'
@@ -47,34 +47,34 @@ def a2():
         BC = '\033[96m'
         BW = '\033[97m'
         GR = '\033[90m'
-    return b1
+    return Colors
 
-if a1():
-    _c = a2()
-    G = _c.BG
-    R = _c.BR
-    C = _c.BC
-    Y = _c.BY
-    W = _c.BW
-    GR = _c.GR
-    B = _c.BB
-    M = _c.BM
-    RS = _c.RS
-    D = _c.D
-    BL = _c.B
+if col_support():
+    _c = get_colors()
+    GREEN = _c.BG
+    RED = _c.BR
+    CYAN = _c.BC
+    YELLOW = _c.BY
+    WHITE = _c.BW
+    GRAY = _c.GR
+    BLUE = _c.BB
+    MAGENTA = _c.BM
+    RESET = _c.RS
+    DIM = _c.D
+    BOLD = _c.B
 else:
-    G = R = C = Y = W = GR = B = M = RS = D = BL = ''
+    GREEN = RED = CYAN = YELLOW = WHITE = GRAY = BLUE = MAGENTA = RESET = DIM = BOLD = ''
 
-def c1(text, code):
-    return f"{code}{text}{RS}" if a1() else text
+def color(text, code):
+    return f"{code}{text}{RESET}" if col_support() else text
 
-def g1(text): return c1(text, G)
-def r1(text): return c1(text, R)
-def c2(text): return c1(text, C)
-def y1(text): return c1(text, Y)
-def w1(text): return c1(text, W)
-def gr1(text): return c1(text, GR)
-def b1(text): return c1(text, B)
-def m1(text): return c1(text, M)
-def d1(text): return f"{D}{text}{RS}" if a1() else text
-def b2(text): return f"{BL}{text}{RS}" if a1() else text
+def green(text): return color(text, GREEN)
+def red(text): return color(text, RED)
+def cyan(text): return color(text, CYAN)
+def yellow(text): return color(text, YELLOW)
+def white(text): return color(text, WHITE)
+def gray(text): return color(text, GRAY)
+def blue(text): return color(text, BLUE)
+def magenta(text): return color(text, MAGENTA)
+def dim(text): return f"{DIM}{text}{RESET}" if col_support() else text
+def bold(text): return f"{BOLD}{text}{RESET}" if col_support() else text
