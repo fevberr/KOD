91ef85d5abd24cdc0b40373b1b084e75e72b6aa6
Fevber <nah.id.win.286h@gmail.com>
2026-07-23 21:31:55 -0600

    re

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from menu import m1
-from display.banner import b1
-from display.panels import p1
-from config import host, port, device, system, ping
 import os
 import sys
 
 def r1():
     os.system('cls' if os.name == 'nt' else 'clear')
-    b1()
-    i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
-    p1("23 KOD", i1, "READY")
+    try:
+        from display.banner import b1
+        from display.panels import p1
+        b1()
+        i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
+        p1("23 KOD", i1, "READY")
+    except:
+        print("+--- 23 KOD")
+        print("| Starting...")
+        print("------------------------------")
     m1()
 
 if __name__ == "__main__":
-    try:
-        from boot import a5
-        a5()
-    except:
-        r1()
+    r1()
