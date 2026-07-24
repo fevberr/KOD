d87cc254d6461888d55d0d89cd5f71b7aa703cf9
Fevber <nah.id.win.286h@gmail.com>
2026-07-24 09:52:14 -0600

    re

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from menu import m1
 import os
 import sys
 
-def r1():
+def a1():
     os.system('cls' if os.name == 'nt' else 'clear')
     try:
         from display.banner import b1
         from display.panels import p1
         b1()
+        from config import host, port, device, system, ping
         i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
         p1("23 KOD", i1, "READY")
     except:
@@ -17,4 +18,8 @@ def r1():
     m1()
 
 if __name__ == "__main__":
-    r1()
+    try:
+        from boot import a11
+        a11()
+    except:
+        a1()
