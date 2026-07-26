e342657840a7eb800aeba1e970d31a8c94b887ff
Fevber <nah.id.win.286h@gmail.com>
2026-07-26 12:23:48 -0600

    re

--- config.py
+++ config.py
@@ -15,16 +15,15 @@ def a1():
 
 
 
-
 TABS = {
+
     "Recon Tab": ["DNS lookup.py", "Port Scanner.py", "Reverse DNS.py", "Shodan lookup.py", "OSINT search.py", "Subdomain enumeration.py", "Whois Lookup.py"],
+
     "Web Enum Tab": ["Directory Brute Force.py", "Parameter Fuzzer.py"],
+
     "Aesthetic": ["Matrix Rain.py", "bad apple.py", "Glitch Effect.py", "test.py", "FPort Scanner.py", "FNetwork Sniffer.py"]
+
 }
-
-
-
-
 
 def a2():
     return f"{platform.system()} {platform.release()}"
