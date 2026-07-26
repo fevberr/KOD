1617b93d10f35890430910db45b21b72fc357ba0
Fevber <nah.id.win.286h@gmail.com>
2026-07-26 12:25:05 -0600

    re

--- utils/colors.py
+++ utils/colors.py
@@ -1,80 +1,41 @@
 import os
 import sys
 import platform
+import json
+import time
+import re
 
-def a1():
-    if not hasattr(sys.stdout, 'isatty') or not sys.stdout.isatty():
-        return False
-    if os.environ.get('NO_COLOR'):
-        return False
-    if os.environ.get('FORCE_COLOR'):
-        return True
-    term = os.environ.get('TERM', '')
-    if term in ('dumb', 'linux'):
-        return False
-    if platform.system() == 'Windows':
-        try:
-            import ctypes
-            kernel32 = ctypes.windll.kernel32
-            handle = kernel32.GetStdHandle(-11)
-            mode = ctypes.c_ulong()
-            if kernel32.GetConsoleMode(handle, ctypes.byref(mode)):
-                kernel32.SetConsoleMode(handle, mode.value | 0x0004)
-                return True
-            return False
-        except:
-            return False
-    return term not in ('', 'dumb', 'linux')
+COLORS_FILE = "color_settings.json"
 
-def a2():
-    class a3:
-        RS = '\033[0m'
-        B = '\033[1m'
-        D = '\033[2m'
-        BL = '\033[30m'
-        R = '\033[31m'
-        G = '\033[32m'
-        Y = '\033[33m'
-        BLU = '\033[34m'
-        M = '\033[35m'
-        C = '\033[36m'
-        W = '\033[37m'
-        BR = '\033[91m'
-        BG = '\033[92m'
-        BY = '\033[93m'
-        BB = '\033[94m'
-        BM = '\033[95m'
-        BC = '\033[96m'
-        BW = '\033[97m'
-        GR = '\033[90m'
-    return a3
+DEFAULT_COLORS = {
+    "primary": "GREEN",
+    "secondary": "CYAN",
+    "success": "GREEN",
+    "error": "RED",
+    "warning": "YELLOW",
+    "info": "BLUE",
+    "highlight": "MAGENTA",
+    "dim": "GRAY",
+    "prompt": "GREEN",
+    "border": "CYAN"
+}
 
-if a1():
-    _c = a2()
-    GREEN = _c.BG
-    RED = _c.BR
-    CYAN = _c.BC
-    YELLOW = _c.BY
-    WHITE = _c.BW
-    GRAY = _c.GR
-    BLUE = _c.BB
-    MAGENTA = _c.BM
-    RESET = _c.RS
-    DIM = _c.D
-    BOLD = _c.B
-else:
-    GREEN = RED = CYAN = YELLOW = WHITE = GRAY = BLUE = MAGENTA = RESET = DIM = BOLD = ''
+COLOR_CODES = {
+    "BLACK": '\033[30m', "RED": '\033[31m', "GREEN": '\033[32m',
+    "YELLOW": '\033[33m', "BLUE": '\033[34m', "MAGENTA": '\033[35m',
+    "CYAN": '\033[36m', "WHITE": '\033[37m',
+    "BRIGHT_RED": '\033[91m', "BRIGHT_GREEN": '\033[92m',
+    "BRIGHT_YELLOW": '\033[93m', "BRIGHT_BLUE": '\033[94m',
+    "BRIGHT_MAGENTA": '\033[95m', "BRIGHT_CYAN": '\033[96m',
+    "BRIGHT_WHITE": '\033[97m', "GRAY": '\033[90m',
+    "PURPLE": '\033[95m', "ORANGE": '\033[38;5;208m',
+    "PINK": '\033[38;5;201m', "LIME": '\033[38;5;154m',
+    "TEAL": '\033[38;5;37m', "GOLD": '\033[38;5;220m',
+    "SILVER": '\033[38;5;250m', "BROWN": '\033[38;5;130m',
+    "RESET": '\033[0m', "BOLD": '\033[1m', "DIM": '\033[2m'
+}
 
-def a4(text, code):
-    return f"{code}{text}{RESET}" if a1() else text
-
-def green(text): return a4(text, GREEN)
-def red(text): return a4(text, RED)
-def cyan(text): return a4(text, CYAN)
-def yellow(text): return a4(text, YELLOW)
-def white(text): return a4(text, WHITE)
-def gray(text): return a4(text, GRAY)
-def blue(text): return a4(text, BLUE)
-def magenta(text): return a4(text, MAGENTA)
-def dim(text): return f"{DIM}{text}{RESET}" if a1() else text
-def bold(text): return f"{BOLD}{text}{RESET}" if a1() else text
+ALL_COLORS = ["GREEN", "RED", "CYAN", "YELLOW", "BLUE", "MAGENTA", "GRAY", "WHITE", 
+              "BLACK", "BRIGHT_GREEN", "BRIGHT_RED", "BRIGHT_CYAN", "BRIGHT_YELLOW",
+              "BRIGHT_BLUE", "BRIGHT_MAGENTA", "BRIGHT_WHITE",
+              "PURPLE", "
