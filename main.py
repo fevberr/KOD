43fb4c6dbdaaec971b1b5e7c66dcdcf030036991
Fevber <nah.id.win.286h@gmail.com>
2026-07-26 17:59:26 -0600

    re

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import sys
 def a1():
     os.system('cls' if os.name == 'nt' else 'clear')
     try:
-        from display.banner import b1
+        from display.banner import a3 as b1
         from display.panels import p1
         b1()
         from config import host, port, device, system, ping
         i1 = f"host:      {host}\nPort:        {port}\nPing:     {ping}\ndevice:   {device}\nsystem:    {system}"
         p1("23 KOD", i1, "READY")
-    except:
-        print("+--- 23 KOD")
+    except Exception as e:
+        print(f"+--- 23 KOD (Error: {e})")
         print("| Starting...")
         print("------------------------------")
     m1()
