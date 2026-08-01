6109e05cd4467b0abefdb3c86f0f92e93d900733
Fevber <nah.id.win.286h@gmail.com>
2026-08-01 01:35:35 -0600

    re

--- events/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import get_events, get_event, list_events, run_event
-
-__all__ = ['get_events', 'get_event', 'list_events', 'run_event']
--- events/__main__.py
+++
@@ -1,8 +0,0 @@
-from .core import list_events, run_event
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(run_event(sys.argv[1]))
-    else:
-        print("Available events:", ", ".join(list_events()))
