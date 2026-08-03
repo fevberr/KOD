2d1d2151ac93dba53c84c57a4fd71a37a8d013c1
Fevber <nah.id.win.286h@gmail.com>
2026-08-03 00:42:45 -0600

    re

--- config.py
+++ config.py
@@ -14,17 +14,35 @@ def a1():
     return sorted(set(m))
 
 TABS = {
-    "Recon Tab": ["DNS lookup.py", "Port Scanner.py", "Reverse DNS.py", "Shodan lookup.py", "OSINT search.py", "Subdomain enumeration.py", "Whois Lookup.py"],
+    "Aesthetic": [
+        "bad apple.py",
+        "FNetwork Sniffer.py",
+        "FPort Scanner.py",
+        "Glitch Effect.py",
+        "Matrix Rain.py",
+        "test.py"
+    ],
     
+    "Exploit Tab": [
+        "Hash Identifier.py",
+        "JWT decoder.py",
+        "Reverse Shell Generator.py"
+    ],
     
-    "Web Enum Tab": ["Directory Brute Force.py", "Parameter Fuzzer.py"],
+    "Recon Tab": [
+        "DNS lookup.py",
+        "OSINT search.py",
+        "Port Scanner.py",
+        "Reverse DNS.py",
+        "Shodan lookup.py",
+        "Subdomain enumeration.py",
+        "Whois Lookup.py"
+    ],
     
-    
-    "Exploit Tab": ["Hash Identifier.py", "JWT decoder.py", "Reverse Shell Generator.py"],
-  
-    
-    "Aesthetic": ["Matrix Rain.py", "bad apple.py", "Glitch Effect.py", "test.py", "FPort Scanner.py", "FNetwork Sniffer.py"],
-    
+    "Web Enum Tab": [
+        "Directory Brute Force.py",
+        "Parameter Fuzzer.py"
+    ]
 }
 
 def a2():
